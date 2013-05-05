c06607f692cccf6ef3f45481fbfb6b241169f6ca
DerFlash <bte@freenet.de>
2013-05-05 11:47:13 +0200

    * updated to craftbukkit 1.5.2 * prepare release 1.3.4

--- src/main/java/me/derflash/plugins/eggroulette/RouletteChicken.java
+++ src/main/java/me/derflash/plugins/eggroulette/RouletteChicken.java
@@ -1,16 +1,16 @@
 package me.derflash.plugins.eggroulette;
 
 import java.lang.reflect.Field;
-import net.minecraft.server.v1_5_R2.EntityAgeable;
-import net.minecraft.server.v1_5_R2.EntityChicken;
-import net.minecraft.server.v1_5_R2.EntityLiving;
-import net.minecraft.server.v1_5_R2.ItemSeeds;
-import net.minecraft.server.v1_5_R2.ItemStack;
-import net.minecraft.server.v1_5_R2.PathfinderGoalFloat;
-import net.minecraft.server.v1_5_R2.PathfinderGoalRandomLookaround;
-import net.minecraft.server.v1_5_R2.PathfinderGoalRandomStroll;
-import net.minecraft.server.v1_5_R2.PathfinderGoalSelector;
-import net.minecraft.server.v1_5_R2.World;
+import net.minecraft.server.v1_5_R3.EntityAgeable;
+import net.minecraft.server.v1_5_R3.EntityChicken;
+import net.minecraft.server.v1_5_R3.EntityLiving;
+import net.minecraft.server.v1_5_R3.ItemSeeds;
+import net.minecraft.server.v1_5_R3.ItemStack;
+import net.minecraft.server.v1_5_R3.PathfinderGoalFloat;
+import net.minecraft.server.v1_5_R3.PathfinderGoalRandomLookaround;
+import net.minecraft.server.v1_5_R3.PathfinderGoalRandomStroll;
+import net.minecraft.server.v1_5_R3.PathfinderGoalSelector;
+import net.minecraft.server.v1_5_R3.World;
 import org.bukkit.Location;
 
 public class RouletteChicken extends EntityChicken {
